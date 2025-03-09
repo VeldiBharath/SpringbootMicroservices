2cfb4c41d12f5672cb25a1c7ba8db05af52d3d65
Bharath Sri Vardhan Veldi <bharathveldiusa@gmail.com>
2025-03-08 19:56:29 -0500

    invoking github actions

--- catalog-service/src/main/java/com/bharath/catalog_service/ApplicationProperties.java
+++ catalog-service/src/main/java/com/bharath/catalog_service/ApplicationProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 @ConfigurationProperties(prefix = "catalog")  //identifies configuration properties records, has to be declared in main
 // start point(i.e. CatalogServiceApplication to be found. use @enableconfigiration at start point to be found)
 
-//This is to make the pageNo as configurable in application.properties file
+//This is to make the pageNo as configurable in application.properties file.
 public record ApplicationProperties(
         @DefaultValue("10")
         @Min(1)
